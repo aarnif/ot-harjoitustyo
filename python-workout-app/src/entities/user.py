fcdd8df2f8abf4f720efcda689e740f9b5796acd
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-31 21:28:20 +0300

    Add default value to weekly training goal

--- python-workout-app/src/entities/user.py
+++ python-workout-app/src/entities/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, username, password, weekly_training_goal_in_minutes):
+    def __init__(self, username, password, weekly_training_goal_in_minutes = 0):
         self.username = username
         self.password = password
         self.weekly_training_goal_in_minutes = weekly_training_goal_in_minutes
