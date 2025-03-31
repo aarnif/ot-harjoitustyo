3a7dcd44d6530d0a07981c2b53f12ce96900e521
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-31 12:34:45 +0300

    Add User-class

---
+++ python-workout-app/src/entities/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username, password, weekly_training_goal_in_minutes):
+        self.username = username
+        self.password = password
+        self.weekly_training_goal_in_minutes = weekly_training_goal_in_minutes
