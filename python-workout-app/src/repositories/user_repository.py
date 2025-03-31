05ad6c68a43f78f4ee1103a12f7e30b3a521ee69
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-31 13:15:10 +0300

    Add User-repository

---
+++ python-workout-app/src/repositories/user_repository.py
@@ -0,0 +1,41 @@
+from entities.user import User
+from database_connection import get_database_connection
+
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def delete_all(self):
+        cursor = self._connection.cursor()
+        cursor.execute("DELETE FROM users")
+        self._connection.commit()
+
+    def find_all(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("SELECT username, password, weekly_training_goal_in_minutes FROM users")
+
+        rows = cursor.fetchall()
+
+        users = [User(row["username"], row["password"], row["weekly_training_goal_in_minutes"]) for row in rows]
+
+        return users
+    
+    def create(self, user):
+        cursor = self._connection.cursor()
+        
+        cursor.execute(
+            "INSERT INTO users (username, password, weekly_training_goal_in_minutes) VALUES (?, ?, ?)",
+            (user.username, user.password, user.weekly_training_goal_in_minutes)
+        )
+        
+        self._connection.commit()
+        
+        return user
+
+
+user_repository = UserRepository(get_database_connection())
+
+if __name__ == "__main__":
+    user_repository.create(User("test", "password", 600))
