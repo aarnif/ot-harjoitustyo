ea7030681dded269a4e4f1af86f572fb024b1bf1
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-31 21:32:09 +0300

    Add test to default weekly training value

--- python-workout-app/src/tests/repositories/user_repository_test.py
+++ python-workout-app/src/tests/repositories/user_repository_test.py
@@ -7,12 +7,21 @@ from entities.user import User
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
         user_repository.delete_all()
-        self.user_matti = User('matti', 'password', 600)
+        self.user_matti = User("matti", "password", 600)
+        self.user_maija = User("maija", "password")
 
     def test_create_user(self):
         user_repository.create(self.user_matti)
         users = user_repository.find_all()
         self.assertEqual(len(users), 1)
-        self.assertEqual(users[0].username, 'matti')
-        self.assertEqual(users[0].password, 'password')
+        self.assertEqual(users[0].username, "matti")
+        self.assertEqual(users[0].password, "password")
         self.assertEqual(users[0].weekly_training_goal_in_minutes, 600)
+
+    def test_default_weekly_training_goal_value(self):
+        user_repository.create(self.user_maija)
+        users = user_repository.find_all()
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].username, "maija")
+        self.assertEqual(users[0].password, "password")
+        self.assertEqual(users[0].weekly_training_goal_in_minutes, 0)
