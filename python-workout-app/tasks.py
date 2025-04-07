5f0a6a111cd833136334f259695af55d86ad99aa
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-07 10:46:05 +0300

    Add build-command

--- python-workout-app/tasks.py
+++ python-workout-app/tasks.py
@@ -1,6 +1,10 @@
 from invoke import task
 
 @task
+def build(ctx):
+    ctx.run("python3 src/initialize_database.py", pty=True)
+
+@task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
