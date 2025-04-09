f4ee93406fd2c129fad3a1e0b3b1c62708faeedb
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-09 11:43:38 +0300

    Add Workout-entity

---
+++ python-workout-app/src/entities/workout.py
@@ -0,0 +1,6 @@
+class Workout:
+    def __init__(self, user_id, type, duration, created_at):
+        self.user_id = user_id
+        self.type = type
+        self.duration = duration
+        self.created_at = created_at
