cbe76a55f7b6e7409d92e8204a33d1596530ad2d
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-12 10:21:19 +0300

    Add convert to datetime

--- python-workout-app/src/database_connection.py
+++ python-workout-app/src/database_connection.py
@@ -1,6 +1,18 @@
 import os
+from datetime import datetime
 import sqlite3
 from config import DATABASE_FILENAME
+
+# generoitu koodi alkaa
+def adapt_datetime(dt: datetime) -> str:
+    return dt.isoformat(" ")
+
+def convert_datetime(s: bytes) -> datetime:
+    return datetime.fromisoformat(s.decode())
+
+sqlite3.register_adapter(datetime, adapt_datetime)
+sqlite3.register_converter("datetime", convert_datetime)
+# generoitu koodi päättyy
 
 dirname = os.path.dirname(__file__)
 
