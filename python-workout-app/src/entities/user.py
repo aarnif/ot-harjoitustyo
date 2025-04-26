a26c41b3f402ccdef47c21f05c305b4b55cf42f2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-26 11:40:55 +0300

    Add docstrings to User-entity

--- python-workout-app/src/entities/user.py
+++ python-workout-app/src/entities/user.py
@@ -1,5 +1,19 @@
 class User:
+    """Luokka, joka kuvaa yksittäistä käyttäjää.
+
+    Attributes:
+        username (str): Käyttäjänimi
+        password (str): Käyttäjän salasana
+        weekly_training_goal_in_minutes (int): Käyttäjän viikoittainen treenitavoite minuutteina
+    """
     def __init__(self, username, password, weekly_training_goal_in_minutes=0):
+        """Luokan konstruktori, joka luo uuden käyttäjän.
+
+        Args:
+            username (str): Käyttäjänimi
+            password (str): Käyttäjän salasana
+            weekly_training_goal_in_minutes (int, optional): Käyttäjän viikoittainen treenitavoite minuutteina. Oletusarvo 0.
+        """
         self.username = username
         self.password = password
         self.weekly_training_goal_in_minutes = weekly_training_goal_in_minutes
