a420615da9e664949fbe0b05e8c393107c88226e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-26 11:41:05 +0300

    Add docstrings to Workout-entity

--- python-workout-app/src/entities/workout.py
+++ python-workout-app/src/entities/workout.py
@@ -1,5 +1,23 @@
 class Workout:
+    """Luokka, joka kuvaa käyttäjän yksittäistä treeniä.
+
+    Attributes:
+        id (int): Treenin id
+        username (str): Käyttäjänimi, joka on treenin luoja
+        type (str): Treenin tyyppi
+        duration (int): Treenin kesto minuutteina
+        created_at (datetime): Treenin luontiaika
+    """
     def __init__(self, username, workout_type, workout_duration, created_at, workout_id=None):
+        """Luokan konstruktori, joka luo uuden treenin.
+
+        Args:
+            username (str): Käyttäjänimi, joka on treenin luoja
+            workout_type (str): Treenin tyyppi
+            workout_duration (str): Treenin kesto minuutteina
+            created_at (str): Treenin luontiaika
+            workout_id (int, optional): Treenin id. Oletusarvo None, ennen kuin treeni tallennetaan tietokantaan.
+        """
         self.id = workout_id
         self.username = username
         self.type = workout_type
